928a8c465609203a8ec6f538ab97ccdab820481e
Nikita Bobylev <nikita@MacBook-Pro-Nikita.local>
2025-11-15 18:21:52 +0300

    add athlete data

--- project_run/apps/athletes/serializers.py
+++ project_run/apps/athletes/serializers.py
@@ -5,8 +5,8 @@ from project_run.apps.athletes.models import AthleteInfo, validate_weight
 
 class AthleteInfoSerializer(ModelSerializer):
     user_id = IntegerField(read_only=True)
-    goals = IntegerField()
-    weight = IntegerField(validators=[validate_weight])
+    goals = IntegerField(default=0)
+    weight = IntegerField(validators=[validate_weight], default=1)
 
 
     class Meta:
