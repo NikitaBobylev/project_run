0b7fa50f60d7eafd270e235214398d12bf7a8b76
Nikita Bobylev <nikita@MacBook-Pro-Nikita.local>
2025-11-15 18:53:40 +0300

    change type of goal

--- project_run/apps/athletes/serializers.py
+++ project_run/apps/athletes/serializers.py
@@ -1,11 +1,11 @@
-from rest_framework.serializers import ModelSerializer, IntegerField, FloatField
+from rest_framework.serializers import ModelSerializer, IntegerField, CharField
 
 from project_run.apps.athletes.models import AthleteInfo, validate_weight
 
 
 class AthleteInfoSerializer(ModelSerializer):
     user_id = IntegerField(read_only=True)
-    goals = FloatField(required=False)
+    goals = CharField(required=False)
     weight = IntegerField(validators=[validate_weight], required=False)
 
 
